b33176170ad3988151fab9437a93f4cba79b6d09
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-12 08:32:10 +0300

    🔨 refactor(expires.rs): Make Expires field contain an Option<Time> This change updates the `Expires` field to contain an `Option<Time>` instead of an empty struct. This allows for more flexibility in the implementation of the `Expires` field by allowing it to be initialized with an expiration time or without one. The `new` function has been updated to accept an argument that implements `Into<Option<Time>>>` and the `Default` implementation sets `Expires` to a default value of `None`. Changes made: - `Expires` struct now contains an `Option<Time>` field instead of an empty struct. - `new` function now accepts an argument that implements `Into<Option<Time>>>` to set the expiration time. - `into_inner` function is added to return the inner value of the `Expires` field. - `Default` implementation sets `Expires` to a default value of `None`. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/domain/clip/field/expires.rs
+++ src/lib/domain/clip/field/expires.rs
@@ -1,3 +1,20 @@
+use crate::domain::time::Time;
 use serde::{Deserialize, Serialize};
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Expires {}
+pub struct Expires(Option<Time>);
+
+impl Expires {
+    pub fn new<T: Into<Option<Time>>>(expires: T) -> Self {
+        Self(expires.into())
+    }
+    pub fn into_inner(self) -> Option<Time> {
+        self.0
+    }
+}
+
+impl Default for Expires {
+    fn default() -> Self {
+        Self::new(None)
+    }
+}
