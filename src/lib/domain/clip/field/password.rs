208f1b940c714fc454d812eef142d72043ed6e8e
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-12 07:09:48 +0300

    🧪 test(password.rs): Add unit tests for Password field Unit tests were added to the `password.rs` file to ensure that the `Password` field type is working correctly. Tests were added for the following functions: - `new()`: Tests whether the `Password` field is initialized correctly with valid and invalid inputs. - `default()`: Tests whether the `Password` field is initialized with default values correctly. - `FromStr::from_str()`: Tests whether the `Password` field is created from a string input correctly with valid and invalid inputs. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/domain/clip/field/password.rs
+++ src/lib/domain/clip/field/password.rs
@@ -39,3 +39,46 @@ impl std::str::FromStr for Password {
         Self::new(s.to_string())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_password() {
+        // Test with valid password
+        let password = Password::new("password123".to_string()).unwrap();
+        assert_eq!(password.has_password(), true);
+        assert_eq!(password.into_inner().unwrap(), "password123".to_string());
+
+        // Test with empty password
+        let password = Password::new("".to_string()).unwrap();
+        assert_eq!(password.has_password(), false);
+        assert_eq!(password.into_inner(), None);
+
+        // Test with None
+        let password = Password::new(None).unwrap();
+        assert_eq!(password.has_password(), false);
+        assert_eq!(password.into_inner(), None);
+    }
+
+    #[test]
+    fn test_default_password() {
+        let password = Password::default();
+        assert_eq!(password.has_password(), false);
+        assert_eq!(password.into_inner(), None);
+    }
+
+    #[test]
+    fn test_from_str_password() {
+        // Test with valid password
+        let password: Password = "password123".parse().unwrap();
+        assert_eq!(password.has_password(), true);
+        assert_eq!(password.into_inner().unwrap(), "password123");
+
+        // Test with empty password
+        let password: Password = "".parse().unwrap();
+        assert_eq!(password.has_password(), false);
+        assert_eq!(password.into_inner(), None);
+    }
+}
