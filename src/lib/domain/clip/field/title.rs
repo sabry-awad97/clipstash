2a4ef1be28bb9ab7b693d0e280ace438afacb11a
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-12 07:32:22 +0300

    🎨 refactor(clip/field/title.rs): Add validation to title field The `Title` struct now includes validation for the `title` field to ensure it is not empty or only contains whitespace. The `new` method now takes a generic type argument that implements `Into<Option<String>>`. This allows for easier construction of the struct from optional strings. The `into_inner` method has been added to retrieve the inner string value from the struct. Additionally, a new `FromStr` implementation has been added to allow the `Title` struct to be created from a `&str`.
    Changes made:
    - The `Title` struct now includes a wrapped `Option<String>` value, which is validated upon construction.
    - A `new` method now takes a generic argument that implements `Into<Option<String>>`. This allows for easy construction of the struct from optional strings.
    - The `into_inner` method has been added to retrieve the inner string value from the struct.
    - A `FromStr` implementation has been added to allow the `Title` struct to be created from a `&str`.
    - A new `ClipError` type has been added to handle invalid `Title` input.
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/domain/clip/field/title.rs
+++ src/lib/domain/clip/field/title.rs
@@ -1,3 +1,37 @@
+use crate::domain::clip::ClipError;
 use serde::{Deserialize, Serialize};
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Title {}
+pub struct Title(Option<String>);
+
+impl Title {
+    pub fn new<T: Into<Option<String>>>(title: T) -> Self {
+        match title.into() {
+            Some(title) => {
+                if !title.trim().is_empty() {
+                    Self(Some(title))
+                } else {
+                    Self(None)
+                }
+            }
+            None => Self(None),
+        }
+    }
+
+    pub fn into_inner(self) -> Option<String> {
+        self.0
+    }
+}
+
+impl Default for Title {
+    fn default() -> Self {
+        Self::new(None)
+    }
+}
+
+impl std::str::FromStr for Title {
+    type Err = ClipError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self::new(s.to_string()))
+    }
+}
