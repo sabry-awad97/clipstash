760fabaa3265b73b979466d61ec8269f0974a67e
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-12 09:10:17 +0300

    🧪 test(posted.rs): Add test for Posted field A new test case was added to test the `into_inner` method for the `Posted` field. This ensures that the `into_inner` method returns the expected `Time` value that was set during initialization. Changes made: - A new test module was added with a test function to test the `into_inner` method for `Posted` field. - The test function creates a new `Time` object, initializes a new `Posted` object with the `Time` object, and checks that the `into_inner` method returns the expected `Time` object. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/domain/clip/field/posted.rs
+++ src/lib/domain/clip/field/posted.rs
@@ -10,3 +10,19 @@ impl Posted {
         self.0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+    use crate::domain::time::Time;
+
+    #[test]
+    fn test_into_inner() {
+        let time_str = "1997-05-01";
+        let time = Time::from_str(time_str).unwrap();
+        let posted = Posted::new(time.clone());
+        assert_eq!(posted.into_inner(), time);
+    }
+}
