0f40ab15153e62f3b302e11b46ff945a5dcb6fff
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-12 09:07:13 +0300

    🚧 refactor(posted.rs): Add constructor and inner function to Posted struct The `Posted` struct in `src/lib/domain/clip/field/posted.rs` currently has an empty body and is not being used. To add functionality to the struct, a `Time` field was added and a constructor was defined using the `derive_more` crate. A new function `into_inner()` was also defined to extract the inner `Time` value from the `Posted` struct. Changes made: - `Time` was imported from `crate::domain::time::Time`. - The `derive_more::Constructor` trait was added to the `Posted` struct to define a constructor that takes a `Time` parameter. - The `Posted` struct now has a `Time` field. - A new function `into_inner()` was added to the `Posted` struct to extract the inner `Time` value. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/domain/clip/field/posted.rs
+++ src/lib/domain/clip/field/posted.rs
@@ -1,3 +1,12 @@
+use crate::domain::time::Time;
+use derive_more::Constructor;
 use serde::{Deserialize, Serialize};
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Posted {}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Constructor)]
+pub struct Posted(Time);
+
+impl Posted {
+    pub fn into_inner(self) -> Time {
+        self.0
+    }
+}
