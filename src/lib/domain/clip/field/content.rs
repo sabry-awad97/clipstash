3a55fe0a4dda7e3dd67b6dd167c9e9db5d81a14c
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-12 06:39:58 +0300

    🐛 fix(content.rs): Add validation to Content field ✨ feat(content.rs): Implement `from_str`, `into_inner`, and `as_str` methods This change adds validation to the `Content` field to ensure that it is not empty. An `EmptyContent` error is returned if the content is empty. Additionally, new methods are added to the `Content` struct: `from_str`, `into_inner`, and `as_str`. `from_str` creates a new `Content` struct from a `&str`. `into_inner` retrieves the inner `String` from a `Content` struct, and `as_str` returns a reference to the inner `String`. Changes made: - The `Content` struct is now wrapped in a `String` to add validation. - A new `from_str` method has been added to create a new `Content` struct from a `&str`. - A new `into_inner` method has been added to consume the inner `String` from a `Content` struct and returns its value. - A new `as_str` method has been added to return a reference to the inner `String`. - If the content is empty, an `EmptyContent` error is returned. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/domain/clip/field/content.rs
+++ src/lib/domain/clip/field/content.rs
@@ -1,3 +1,21 @@
+use crate::domain::clip::ClipError;
 use serde::{Deserialize, Serialize};
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Content {}
+pub struct Content(String);
+
+impl Content {
+    pub fn from_str(content: &str) -> Result<Self, ClipError> {
+        if !content.trim().is_empty() {
+            Ok(Self(content.to_owned()))
+        } else {
+            Err(ClipError::EmptyContent)
+        }
+    }
+    pub fn into_inner(self) -> String {
+        self.0
+    }
+    pub fn as_str(&self) -> &str {
+        self.0.as_str()
+    }
+}
