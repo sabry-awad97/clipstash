1a38afd5ed749be4b472cae3664e77417621a8ab
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-12 07:01:15 +0300

    🔒 refactor(password.rs): Add validation logic to password field The `Password` field struct in the domain model did not have any validation logic to ensure the password was not empty or consisted of only whitespace. This could result in invalid data being created in the system. To fix this, validation logic was added to the `new` constructor method. Changes made: - The `Password` struct now has a single field that is an optional `String`. - The `new` constructor method now takes an input of type `T: Into<Option<String>>`, which can be either `Some(String)` or `None`. - If the input is `Some(password)`, the password is checked for validity, and if it is valid, the `Password` struct with the password field set to `Some(password)` is returned. - If the input is `None` or the password is invalid, the `Password` struct with the password field set to `None` is returned. - An `Err(ClipError)` is returned if the password is invalid. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/domain/clip/field/password.rs
+++ src/lib/domain/clip/field/password.rs
@@ -1,3 +1,20 @@
+use crate::domain::clip::ClipError;
 use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Password {}
+pub struct Password(Option<String>);
+
+impl Password {
+    pub fn new<T: Into<Option<String>>>(password: T) -> Result<Self, ClipError> {
+        let password: Option<String> = password.into();
+        match password {
+            Some(password) => {
+                if !password.trim().is_empty() {
+                    Ok(Self(Some(password)))
+                } else {
+                    Ok(Self(None))
+                }
+            }
+            None => Ok(Self(None)),
+        }
+    }
+}
