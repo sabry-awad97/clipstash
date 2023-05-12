605b3ded7ea2888f0080411e7e5fd295ed6dbdca
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-12 06:54:07 +0300

    🧪 test(hits.rs): Add tests for Hits struct This code change adds two tests to the `Hits` struct in `hits.rs`. The tests verify the functionality of `Hits::new()` and `Hits::into_inner()`. Changes made: - Two new tests were added to the `tests` module of `hits.rs`.   - `test_hits_new` tests the `new()` function of `Hits`.   - `test_hits_into_inner` tests the `into_inner()` function of `Hits`. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/domain/clip/field/hits.rs
+++ src/lib/domain/clip/field/hits.rs
@@ -9,3 +9,20 @@ impl Hits {
         self.0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hits_new() {
+        let hits = Hits::new(10);
+        assert_eq!(hits.into_inner(), 10);
+    }
+
+    #[test]
+    fn test_hits_into_inner() {
+        let hits = Hits::new(5);
+        assert_eq!(hits.into_inner(), 5);
+    }
+}
