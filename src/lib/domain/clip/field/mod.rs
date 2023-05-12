51a5763002af4f9847fdedaf518f7b5129dc5e30
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-12 05:51:11 +0300

    🎉 feat(domain): Add content field to Clip struct A new field called `Content` was added to the `Clip` struct under the `domain/clip/field` module. A new module called `content` was created to contain the `Content` struct. Changes made: - A new file `content.rs` was created in the `domain/clip/field` module containing the `Content` struct. - The `mod.rs` file in the `domain/clip/field` module was updated to export the new `Content` struct. - The `Clip` struct in the `domain/clip` module was updated to include the new `content` field of type `field::Content`. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/domain/clip/field/mod.rs
+++ src/lib/domain/clip/field/mod.rs
@@ -3,3 +3,6 @@ pub use clip_id::ClipId;
 
 mod shortcode;
 pub use shortcode::ShortCode;
+
+mod content;
+pub use content::Content;
--- src/lib/domain/clip/mod.rs
+++ src/lib/domain/clip/mod.rs
@@ -3,4 +3,5 @@ pub mod field;
 pub struct Clip {
     pub clip_id: field::ClipId,
     pub shortcode: field::ShortCode,
+    pub content: field::Content,
 }
