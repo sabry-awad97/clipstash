33da2f891a15cc5a1d94f055b060d05a2fbbe316
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-12 08:34:34 +0300

    🚀 feat(expires.rs): Implement `FromStr` trait for `Expires` The `FromStr` trait is implemented to allow parsing of `Expires` field from a string. The `FromStr` implementation handles both empty and non-empty strings. If the string is empty, the function returns an `Expires` object with a `None` value. If the string is non-empty, the function tries to parse it as a `Time` object and returns an `Expires` object with the parsed value. If the parsing fails, the function returns a `ClipError`. Changes made: - Added `use` statement for `ClipError`. - Implemented `FromStr` trait for `Expires`. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/domain/clip/field/expires.rs
+++ src/lib/domain/clip/field/expires.rs
@@ -1,5 +1,6 @@
-use crate::domain::time::Time;
+use crate::domain::{clip::ClipError, time::Time};
 use serde::{Deserialize, Serialize};
+use std::str::FromStr;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Expires(Option<Time>);
@@ -16,5 +17,19 @@ impl Expires {
 impl Default for Expires {
     fn default() -> Self {
         Self::new(None)
+    }
+}
+
+impl FromStr for Expires {
+    type Err = ClipError;
+    fn from_str(raw: &str) -> Result<Self, Self::Err> {
+        if raw.is_empty() {
+            Ok(Self(None))
+        } else {
+            match Time::from_str(raw) {
+                Ok(time) => Ok(Self::new(time)),
+                Err(e) => Err(e.into()),
+            }
+        }
     }
 }
