c2be36a6084ec07a267a0a1b2aa49463e913cbd6
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-12 07:15:41 +0300

    🚀 feat(Cargo.toml): Add `rand` dependency for shortcode generation 🚀 feat(src/lib/domain/clip/field/shortcode.rs): Add implementation for ShortCode generation using `rand` The `rand` crate was added as a dependency in `Cargo.toml` to facilitate generating random shortcodes. The `ShortCode` struct now has an implementation for generating new shortcodes. A `new()` method was added which generates a new shortcode using `rand::thread_rng()` and `rand::seq::SliceRandom`. The shortcode is a 10 character string consisting of a combination of letters and numbers. Changes made: - The `rand` crate was added to `Cargo.toml`. - The `ShortCode` struct was modified to include a `new()` method which generates a new shortcode. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/domain/clip/field/shortcode.rs
+++ src/lib/domain/clip/field/shortcode.rs
@@ -1,3 +1,21 @@
+use rand::{seq::SliceRandom, thread_rng};
 use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ShortCode {}
+pub struct ShortCode(String);
+
+impl ShortCode {
+    pub fn new() -> Self {
+        let allowed_chars = ['a', 'b', 'c', 'd', '1', '2', '3', '4'];
+
+        let mut rng = thread_rng();
+        let mut shortcode = String::with_capacity(10);
+        for _ in 0..10 {
+            shortcode.push(
+                *allowed_chars
+                    .choose(&mut rng)
+                    .expect("sampling array should have values"),
+            );
+        }
+        Self(shortcode)
+    }
+}
