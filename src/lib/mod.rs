762383fcd4c5c8b3188b0961ae6f7f70d711268e
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-14 16:08:38 +0300

    👌 refactor(lib/mod.rs): Expose types through public use This change adds several types to the public interface of the library. The `use` statements are moved to the bottom of the file and the types are then exposed through public use. Changes made: - Types `DataError`, `ShortCode`, `Clip`, `ClipError`, and `Time` are now exposed through public use. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -2,3 +2,8 @@ pub mod data;
 pub mod domain;
 pub mod service;
 pub mod web;
+
+pub use data::DataError;
+pub use domain::clip::field::ShortCode;
+pub use domain::clip::{Clip, ClipError};
+pub use domain::time::Time;
