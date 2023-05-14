4ddba7cd6ce9f2f7911ef7f3812dbe93e533ff7f
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-14 18:13:27 +0300

    🧪 test(query.rs): Add test for get_clip function This commit adds a test for the `get_clip` function in `query.rs`. The test initializes an in-memory SQLite database, inserts a clip, and then queries the database for the clip by its shortcode. The test then checks that the returned clip has the same clip ID as the inserted clip. Changes made: - A new `mod` for testing was added. - An `initialize` function was added to set up an in-memory SQLite database and run migrations. - A new test function `test_get_clip` was added to test the `get_clip` function. - The `insert_clip` function was added to insert a clip into the database for testing purposes. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/data/query.rs
+++ src/lib/data/query.rs
@@ -18,3 +18,75 @@ pub async fn get_clip<M: Into<model::GetClip>>(
     .fetch_one(pool)
     .await?)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::data::model::Clip;
+    use chrono::NaiveDateTime;
+    use sqlx::{Pool, Sqlite};
+
+    const CLIP_ID: &str = "01234567-89ab-cdef-0123-456789abcdef";
+
+    async fn initialize() -> Pool<Sqlite> {
+        let pool = Pool::connect("sqlite::memory:").await.unwrap();
+        sqlx::migrate!("./migrations").run(&pool).await.unwrap();
+        pool
+    }
+
+    #[tokio::test]
+    async fn test_get_clip() {
+        let pool = initialize().await;
+        let clip = Clip {
+            clip_id: CLIP_ID.to_string(),
+            shortcode: "abc123".to_string(),
+            content: "Hello, world!".to_string(),
+            title: Some("Test Clip".to_string()),
+            posted: NaiveDateTime::from_timestamp_opt(862070800, 0).unwrap(),
+            expires: NaiveDateTime::from_timestamp_opt(862060800, 0),
+            password: Some("password".to_string()),
+            hits: 10,
+        };
+
+        insert_clip(&clip, &pool).await.unwrap();
+
+        // Test getting the clip by shortcode
+        let result = get_clip(
+            model::GetClip {
+                shortcode: clip.shortcode.clone(),
+            },
+            &pool,
+        )
+        .await
+        .unwrap();
+        assert_eq!(result.clip_id, clip.clip_id);
+    }
+
+    async fn insert_clip(clip: &Clip, pool: &Pool<Sqlite>) -> sqlx::Result<()> {
+        sqlx::query!(
+            r#"INSERT INTO
+                clips (
+                    clip_id,
+                    shortcode,
+                    content,
+                    title,
+                    posted,
+                    expires,
+                    password,
+                    hits
+                )
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)"#,
+            clip.clip_id,
+            clip.shortcode,
+            clip.content,
+            clip.title,
+            clip.posted,
+            clip.expires,
+            clip.password,
+            clip.hits
+        )
+        .execute(pool)
+        .await
+        .map(|_| ())
+    }
+}
