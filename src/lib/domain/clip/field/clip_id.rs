82bcd626f78a4906fbc393e5ae6cec418fa48ef2
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-14 05:38:24 +0300

    🦀 chore(clip_id.rs): Implement newtype pattern for ClipId This change uses the newtype pattern for the `ClipId` struct to wrap the existing `DbId` type. This is done using the `derive_more::Constructor` macro, which generates a constructor method for the `ClipId` struct that takes a `DbId` argument. Additionally, a new `into_inner` method is added to extract the wrapped `DbId` value, and an implementation of the `From` trait is added to convert a `DbId` into a `ClipId`. Finally, a `Default` implementation is added that returns a new `ClipId` instance with a `DbId` value of `nil()`. Changes made: - `use` statement added for the `derive_more::Constructor` macro. - `ClipId` struct is changed to use `DbId` field wrapped using newtype pattern. - `into_inner` method added to extract the wrapped `DbId` value. - `From` trait implementation added to convert a `DbId` into a `ClipId`. - `Default` trait implementation added to return a new `ClipId` instance with a `DbId` value of `nil()`. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/domain/clip/field/clip_id.rs
+++ src/lib/domain/clip/field/clip_id.rs
@@ -1,3 +1,24 @@
+use derive_more::Constructor;
 use serde::{Deserialize, Serialize};
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ClipId {}
+
+use crate::data::DbId;
+#[derive(Debug, Clone, Constructor, Serialize, Deserialize)]
+pub struct ClipId(DbId);
+
+impl ClipId {
+    pub fn into_inner(self) -> DbId {
+        self.0
+    }
+}
+
+impl From<DbId> for ClipId {
+    fn from(id: DbId) -> Self {
+        Self(id)
+    }
+}
+
+impl Default for ClipId {
+    fn default() -> Self {
+        Self(DbId::nil())
+    }
+}
