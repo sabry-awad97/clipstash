2c5f4bdada6f1d85c2a390b05622d828f1aa8652
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-14 05:52:27 +0300

    🧪 test(data/mod.rs): Add tests for DbId This change adds tests for the `DbId` type in `data/mod.rs`. The tests check that `DbId` can be created with a new UUID, with a nil UUID, and from a UUID string. Additionally, tests check that `DbId` can be converted to a string, and that different `DbId` values are not equal. Changes made: - `#[derive(PartialEq)]` was added to the `DbId` struct to enable equality checks in the tests. - Tests were added to check the `new()`, `nil()`, `from_str()`, and `Display` implementations of `DbId`. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/data/mod.rs
+++ src/lib/data/mod.rs
@@ -4,7 +4,7 @@ use derive_more::{Display, From};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Clone, Debug, From, Display, Deserialize, Serialize)]
+#[derive(Clone, Debug, From, Display, Deserialize, Serialize, PartialEq)]
 pub struct DbId(Uuid);
 
 impl DbId {
@@ -33,5 +33,37 @@ impl FromStr for DbId {
     type Err = uuid::Error;
     fn from_str(id: &str) -> Result<Self, Self::Err> {
         Ok(DbId(Uuid::parse_str(id)?))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_db_id() {
+        let id1 = DbId::new();
+        let id2 = DbId::new();
+        assert_ne!(id1, id2);
+    }
+
+    #[test]
+    fn test_nil_db_id() {
+        let id = DbId::nil();
+        assert_eq!(id, DbId(Uuid::nil()));
+    }
+
+    #[test]
+    fn test_db_id_from_string() {
+        let id_str = "01234567-89ab-cdef-0123-456789abcdef";
+        let id = DbId::from_str(id_str).unwrap();
+        assert_eq!(id, DbId(Uuid::parse_str(id_str).unwrap()));
+    }
+
+    #[test]
+    fn test_db_id_to_string() {
+        let id_str = "01234567-89ab-cdef-0123-456789abcdef";
+        let id = DbId::from_str(id_str).unwrap();
+        assert_eq!(String::from(id), id_str);
     }
 }
