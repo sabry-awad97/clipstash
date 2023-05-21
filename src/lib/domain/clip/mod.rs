41e844c2662d57094837755394062f8bb549d657
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-21 22:37:43 +0300

    🚧 chore(clip): Add unit test for Clip struct A unit test has been added to ensure the correctness of the `Clip` struct in the `src/lib/domain/clip/mod.rs` file.
    Changes made:
    - Created a new module `tests` within the `clip` module.
    - Imported necessary dependencies and modules.
    - Added a unit test function `test_clip` to validate the properties of the `Clip` struct.
    - Created test data and instantiated a `Clip` object with the test data.
    - Asserted the equality of each property against the expected values.
    
    This unit test ensures that the `Clip` struct functions as intended and that its properties are correctly initialized.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/domain/clip/mod.rs
+++ src/lib/domain/clip/mod.rs
@@ -37,3 +37,46 @@ pub struct Clip {
     pub password: field::Password,
     pub hits: field::Hits,
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{data::DbId, Time};
+
+    use super::*;
+    use std::{convert::TryFrom, str::FromStr};
+
+    #[test]
+    fn test_clip() {
+        let db_id: DbId = DbId::new();
+        let clip_id = field::ClipId::new(db_id.clone());
+        let shortcode = field::ShortCode::try_from("abc123").unwrap();
+        let content = field::Content::new("Hello, world!").unwrap();
+        let title = field::Title::new("My Clip".to_string());
+        let time_str = "1997-05-01";
+        let time = Time::from_str(time_str).unwrap();
+        let posted = field::Posted::new(time.clone());
+        let expires = field::Expires::new(Some(Time::from_seconds(3600)));
+        let password = field::Password::new("password123".to_string()).unwrap();
+        let hits = field::Hits::new(0);
+
+        let clip = Clip {
+            clip_id: clip_id.clone(),
+            shortcode: shortcode.clone(),
+            content: content.clone(),
+            title: title.clone(),
+            posted: posted.clone(),
+            expires: expires.clone(),
+            password: password.clone(),
+            hits: hits.clone(),
+        };
+
+        assert_eq!(clip.clip_id, clip_id);
+        assert_eq!(clip.shortcode, shortcode);
+        assert_eq!(clip.content, content);
+        assert_eq!(clip.title, title);
+        assert_eq!(clip.posted, posted);
+        assert_eq!(clip.expires, expires);
+        assert_eq!(clip.password, password);
+        assert_eq!(clip.hits, hits);
+    }
+}
