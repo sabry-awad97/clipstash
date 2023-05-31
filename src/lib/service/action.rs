35fbb0bfea40fe1cbc8d1595f51269fe3406d9f8
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-31 13:32:36 +0300

    📝 chore(action.rs): Add function documentation for clip actions This commit adds function documentation for the clip actions in the `action.rs` file. The functions `new_clip`, `update_clip`, and `get_clip` now have detailed explanations of their purpose, arguments, and return values. Changes made: - Added function documentation for `new_clip`, `update_clip`, and `get_clip` functions. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/service/action.rs
+++ src/lib/service/action.rs
@@ -3,14 +3,47 @@ use crate::service::ask;
 use crate::{Clip, ServiceError};
 use std::convert::TryInto;
 
+/// Creates a new clip based on the provided request and inserts it into the database.
+///
+/// # Arguments
+///
+/// * `req` - The request object containing necessary information to create the clip.
+/// * `pool` - A reference to the database connection pool.
+///
+/// # Returns
+///
+/// A `Result` indicating either the newly created `Clip` or a `ServiceError` if an error occurs.
+///
 pub async fn new_clip(req: ask::NewClip, pool: &DatabasePool) -> Result<Clip, ServiceError> {
     Ok(query::insert_clip(req, pool).await?.try_into()?)
 }
 
+/// Updates an existing clip based on the provided request and updates it in the database.
+///
+/// # Arguments
+///
+/// * `req` - The request object containing necessary information to update the clip.
+/// * `pool` - A reference to the database connection pool.
+///
+/// # Returns
+///
+/// A `Result` indicating either the updated `Clip` or a `ServiceError` if an error occurs.
+///
 pub async fn update_clip(req: ask::UpdateClip, pool: &DatabasePool) -> Result<Clip, ServiceError> {
     Ok(query::update_clip(req, pool).await?.try_into()?)
 }
 
+/// Retrieves a clip based on the provided request and database connection pool.
+///
+/// # Arguments
+///
+/// * `req` - The request object containing necessary information to retrieve the clip.
+/// * `pool` - A reference to the database connection pool.
+///
+/// # Returns
+///
+/// A `Result` indicating either the retrieved `Clip` or a `ServiceError` if an error occurs.
+///
 pub async fn get_clip(req: ask::GetClip, pool: &DatabasePool) -> Result<Clip, ServiceError> {
     let user_password = req.password.clone();
     let clip: Clip = query::get_clip(req, pool).await?.try_into()?;
