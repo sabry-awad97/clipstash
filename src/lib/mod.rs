ca1c720c001fd548565829f05b894ad1ccb66552
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-31 19:11:21 +0300

    🎉 feat(lib): Add Rocket configuration and static file handling 📦 chore(static): Add clipstash.css file
    This commit introduces the following changes:
    
    In `src/lib/mod.rs`:
    - Added a `RocketConfig` struct to hold configuration data for Rocket.
    - Added a `rocket` function that takes a `RocketConfig` and returns a configured `Rocket` instance.
    - Registered `AppDatabase` and `Renderer` as managed state in the `Rocket` instance.
    - Mounted routes for the root path ("/"), static files ("/static"), and error handlers.
    
    In `static/clipstash.css`:
    - Added a new file `clipstash.css` to the static directory.
    
    These changes enable Rocket configuration and static file serving for the application.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -8,3 +8,22 @@ pub use domain::clip::field::ShortCode;
 pub use domain::clip::{Clip, ClipError};
 pub use domain::time::Time;
 pub use service::ServiceError;
+
+use data::AppDatabase;
+use rocket::fs::FileServer;
+use rocket::{Build, Rocket};
+use web::renderer::Renderer;
+
+pub struct RocketConfig {
+    pub renderer: Renderer<'static>,
+    pub database: AppDatabase,
+}
+
+pub fn rocket(config: RocketConfig) -> Rocket<Build> {
+    rocket::build()
+        .manage::<AppDatabase>(config.database)
+        .manage::<Renderer>(config.renderer)
+        .mount("/", web::http::routes())
+        .mount("/static", FileServer::from("static"))
+        .register("/", web::http::catchers())
+}
