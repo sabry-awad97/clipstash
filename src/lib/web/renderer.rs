924637d5944ca90af14d7bf8f4f95f6c6b63bea2
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-31 14:34:33 +0300

    🎉 feat(web/renderer.rs): Add do_render function for template rendering
    A new function `do_render` has been added to the `Renderer` struct. This function takes a template name and a context as input and returns the rendered template as a `String`. If successful, it returns the rendered template, and if there is an error during rendering, it returns a `RendererError`.
    
    The purpose of this addition is to provide a convenient way to render templates using the Handlebars library. The `do_render` function internally calls the `render` function from Handlebars and handles any rendering errors by mapping them to the `RendererError::RenderError` variant.
    
    Changes made:
    - Added `do_render` function to the `Renderer` struct.
    - The function takes a template name and a context as parameters.
    - It returns a `Result<String, RendererError>` where the `Ok` variant contains the rendered template and the `Err` variant represents rendering errors.
    
    Tests:
    - Added a test case for the `do_render` function.
    - The test case creates a temporary directory and a template file.
    - It writes a template content to the file.
    - The `do_render` function is called with the template name and a context.
    - The result is asserted to match the expected rendered output.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/web/renderer.rs
+++ src/lib/web/renderer.rs
@@ -29,11 +29,27 @@ impl<'r> Renderer<'r> {
 
         Ok(Self(handlebars))
     }
+
+    pub fn do_render(
+        &self,
+        template_name: &str,
+        context: &serde_json::Value,
+    ) -> Result<String, RendererError> {
+        let rendered =
+            self.0
+                .render(template_name, context)
+                .map_err(|e| RendererError::RenderError {
+                    template_name: template_name.to_string(),
+                    source: e,
+                })?;
+        Ok(rendered)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde_json::json;
     use std::fs::File;
     use std::io::Write;
     use tempfile::tempdir;
@@ -47,5 +63,19 @@ mod tests {
 
         let renderer = Renderer::new(template_dir.path().to_path_buf()).unwrap();
         assert!(renderer.0.get_template("template").is_some());
+    }
+
+    #[test]
+    fn test_do_render() {
+        let template_dir = tempdir().unwrap();
+        let template_file = template_dir.path().join("template.hbs");
+        let mut file = File::create(&template_file).unwrap();
+        file.write_all(b"Hello, {{name}}!").unwrap();
+
+        let renderer = Renderer::new(template_dir.path().to_path_buf()).unwrap();
+        let result = renderer
+            .do_render("template", &json!({"name": "World"}))
+            .unwrap();
+        assert_eq!(result, "Hello, World!");
     }
 }
