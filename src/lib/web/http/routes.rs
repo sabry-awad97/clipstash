6d4f9fddda3d8fb4b6abc264a42841faa6d5b898
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-02 15:30:22 +0300

    ✨ feat(routes.rs): Add route for retrieving clip with shortcode 🔧 fix(routes.rs): Improve error handling and response for get_clip route
    The `get_clip` route was added to handle requests for retrieving a clip using a shortcode. It interacts with the `AppDatabase` and the `Renderer` to render the appropriate response.
    
    Changes made:
    - Added `get_clip` function to handle requests for `/clip/<shortcode>`.
    - Updated the function signature to include the necessary parameters (`shortcode`, `database`, `renderer`).
    - Implemented the logic to fetch the clip using the `action::get_clip` function.
    - Added a `match` expression to handle different scenarios:
      - If the clip is found, the response with a successful status and the rendered clip is returned.
      - If there is a permission error, the response with an unauthorized status and a password prompt is returned.
      - If the clip is not found, a `PageError` with a not found message is returned.
      - For any other errors, a `PageError` with an internal server error message is returned.
    - Updated the `routes` function to include the `get_clip` route.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/web/http/routes.rs
+++ src/lib/web/http/routes.rs
@@ -1,5 +1,15 @@
-use crate::web::{ctx, renderer::Renderer};
-use rocket::{get, response::content::RawHtml, State};
+use crate::{
+    data::AppDatabase,
+    service::action,
+    web::{ctx, renderer::Renderer, PageError},
+    ServiceError, ShortCode,
+};
+use rocket::{
+    get,
+    http::Status,
+    response::{content::RawHtml, status},
+    State,
+};
 
 #[get("/")]
 fn home(renderer: &State<Renderer<'_>>) -> RawHtml<String> {
@@ -7,6 +17,34 @@ fn home(renderer: &State<Renderer<'_>>) -> RawHtml<String> {
     RawHtml(renderer.render(context, &[]))
 }
 
+#[get("/clip/<shortcode>")]
+pub async fn get_clip(
+    shortcode: ShortCode,
+    database: &State<AppDatabase>,
+    renderer: &State<Renderer<'_>>,
+) -> Result<status::Custom<RawHtml<String>>, PageError> {
+    match action::get_clip(shortcode.clone().into(), database.get_pool()).await {
+        Ok(clip) => {
+            let context = ctx::ViewClip::new(clip);
+            Ok(status::Custom(
+                Status::Ok,
+                RawHtml(renderer.render(context, &[])),
+            ))
+        }
+        Err(e) => match e {
+            ServiceError::PermissionError(_) => {
+                let context = ctx::PasswordRequired::new(shortcode);
+                Ok(status::Custom(
+                    Status::Unauthorized,
+                    RawHtml(renderer.render(context, &[])),
+                ))
+            }
+            ServiceError::NotFound => Err(PageError::NotFound("Clip not found".to_owned())),
+            _ => Err(PageError::Internal("Server Error".to_owned())),
+        },
+    }
+}
+
 pub fn routes() -> Vec<rocket::Route> {
-    rocket::routes![home]
+    rocket::routes![home, get_clip]
 }
