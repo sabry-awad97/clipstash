66abeca58feb3bcca520c21c34c6e0d1c3852e39
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-06 12:51:35 +0300

    🎉 feat(web/api/error.rs): Add additional error handling for API responses
    This commit adds additional error handling functionality to the `web/api/error.rs` module. Specifically, it introduces a new `ApiError` enum to handle various types of API errors that can occur during request processing.
    
    Changes made:
    - Added a new `ApiError` enum to handle different types of API errors.
    - Implemented the `From<ServiceError>` trait for `ApiError` to convert `ServiceError` instances into appropriate `ApiError` variants.
    - Added variant cases for `NotFound`, `Server`, `User`, and `KeyError` errors in the `ApiError` enum.
    - Modified the `impl` block to map specific `ServiceError` instances to corresponding `ApiError` variants.
    
    These changes improve the error reporting and handling capabilities of the API, allowing for more specific error responses to be returned to clients.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/lib/web/api/error.rs
+++ src/lib/web/api/error.rs
@@ -1,5 +1,7 @@
-use rocket::Responder;
+use rocket::{serde::json::Json, Responder};
 use serde::Serialize;
+
+use crate::ServiceError;
 
 #[derive(Responder, Debug, thiserror::Error, Serialize)]
 pub enum ApiKeyError {
@@ -11,4 +13,34 @@ pub enum ApiKeyError {
     #[error("invalid API key format")]
     #[response(status = 400, content_type = "json")]
     DecodeError(String),
+}
+
+#[derive(Responder, Debug, thiserror::Error)]
+pub enum ApiError {
+    #[error("not found")]
+    #[response(status = 404, content_type = "json")]
+    NotFound(Json<String>),
+
+    #[error("server error")]
+    #[response(status = 500, content_type = "json")]
+    Server(Json<String>),
+
+    #[error("client error")]
+    #[response(status = 401, content_type = "json")]
+    User(Json<String>),
+
+    #[error("key error")]
+    #[response(status = 400, content_type = "json")]
+    KeyError(Json<ApiKeyError>),
+}
+
+impl From<ServiceError> for ApiError {
+    fn from(err: ServiceError) -> Self {
+        match err {
+            ServiceError::Clip(c) => Self::User(Json(format!("clip parsing error: {}", c))),
+            ServiceError::NotFound => Self::NotFound(Json("entity not found".to_owned())),
+            ServiceError::Data(_) => Self::Server(Json("a server error occurred".to_owned())),
+            ServiceError::PermissionError(msg) => Self::User(Json(msg)),
+        }
+    }
 }
